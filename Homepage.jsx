9a8e93e4e3c9b3b974ae681d43d729799620def0
David Durika <durika.d@gmail.com>
2015-11-17 10:57:28 +0100

    update [closes #1]

--- Homepage.jsx
+++ Homepage.jsx
@@ -4,7 +4,7 @@ Homepage = React.createClass({
       <div className="home">
         <div className="jumbo">
           <h2><i className="fa fa-heartbeat" /> Dr. Mongo</h2>
-          <h3>MongoDB admin app build on MeteorJs.</h3>
+          <h3>Open-source MongoDB admin app build on MeteorJs.</h3>
           <div className="buttons">
             <a className="btn btn-jumbo" href="http://demo.drmongo.com/"><i className="fa fa-heartbeat" /> Demo</a>
             <a className="btn btn-jumbo" href="https://github.com/DrMongo/DrMongo"><i className="fa fa-github" /> Fork me</a>
--- MainLayout.jsx
+++ MainLayout.jsx
@@ -1,11 +1,21 @@
 MainLayout = React.createClass({
+
+  componentDidMount() {
+    DocHead.setTitle('Dr. Mongo');
+    DocHead.addLink({rel: 'icon', href: '/favicon.ico?v=1.0'});
+  },
+
   render() {
     return (
       <div>
         <div className="text-center">
           {this.props.content}
         </div>
-        <footer>Dr. Mongo</footer>
+        <footer>
+          <div className="m-y">
+            Dr. Mongo
+          </div>
+        </footer>
       </div>
     );
   }
